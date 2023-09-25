69fcb040cafbc5bf3d5f3ca78b33d2413aa78194
michelrubens <michelrube@gmail.com>
2023-09-25 16:48:17 -0300

    erase, clear, merge

--- estruturas/lista.cpp
+++ estruturas/lista.cpp
@@ -16,36 +16,51 @@
 // sort: ordena a lista
 // reverse: inverte a lista
 // insert: insere valor na posição definida (iterador, posição)
+// erase: apaga valor na posição definida (posição)
+// clear: limpa a lista
 // begin: indica primeira posição da lista
+// merge: remove elementos de uma lista e junta com os de outra 
 
 using namespace std;
 
 int main() {
 
-    list <int> mesa;
+    list <int> mesa, teste;
     int qtd;
     list <int> :: iterator it; // iterador
+
+    teste.push_front(9);
+    teste.push_front(9);
+    teste.push_front(9);
+    teste.push_front(9);
 
     qtd=10;
     for(int i=0; i<qtd; i++) {
         mesa.push_front(i); // insere no início
     }
 
-    it=mesa.begin(); // indica primeira posição
-    advance(it,5); // avança o iterador 5 posições
+    it=mesa.begin();
+    advance(it,3); // avanca o iterador para a posicao 3
+    mesa.insert(it,0); // adiciona 0 na posicao do iterador (3)
 
-    mesa.insert(it,0); // insere 0 na posição do iterador
+    mesa.erase(--it); // remove o 0 inserido anteriormente
 
-    mesa.sort(); // ordena a lista (de 0 a 9)
-    mesa.reverse(); // inverte a lista (de 9 a 0)
+    mesa.merge(teste);
+
+    // mesa.sort(); // ordena a lista (de 0 a 9)
+    // mesa.reverse(); // inverte a lista (de 9 a 0)
+
+    // mesa.clear(); // limpa a lista 'mesa'
     
-    cout << "Quantidade de mesas: " << mesa.size() << "\n";
+    cout << "Quantidade de mesas: " << mesa.size() << "\n\n";
 
     qtd=mesa.size();
     for(int i=0; i<qtd; i++) {
         cout << mesa.front() << "\n";
         mesa.pop_front();
     }
+
+    cout << "Quantidade de mesas: " << mesa.size() << "\n";
 
     return 0;
 }
