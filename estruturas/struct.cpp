534a54574314650cbfea92cd5301394966fb47d1
michelrubens <michelrube@gmail.com>
2023-10-04 16:08:43 -0300

    Inclusão de métodos

--- estruturas/struct.cpp
+++ estruturas/struct.cpp
@@ -2,36 +2,56 @@
 
 using namespace std;
 
-struct Carro {
+struct Carro
+{
     string nome;
     string cor;
     int pot;
     int vMax;
+    int vel;
+
+    // Métodos
+    void insere(string stnome, string stcor, int stpot, int stvMax)
+    {
+        nome = stnome;
+        cor = stcor;
+        pot = stpot;
+        vMax = stvMax;
+        vel=0;
+    }
+
+    void mostra()
+    {
+        cout << "Nome.............: " << nome << "\n";
+        cout << "Cor..............: " << cor << "\n";
+        cout << "Potencia.........: " << pot << "\n";
+        cout << "Velocidade atual.: " << vel << "\n";
+        cout << "Velocidade maxima: " << vMax << "\n";
+    }
+
+    void mudaVel(int mv)
+    {
+        vel=mv;
+        if (vel > vMax)
+        {
+            vel=vMax;
+        }
+        if (vel < 0)
+        {
+            vel=0;
+        }
+    }
 };
 
-int main() {
+int main()
+{
+    Carro car1, car2;
 
-    Carro car, car2;
+    car1.insere("Tornado", "Vermelho", 450, 350);
+    car2.insere("Monza", "Cinza", 250, 260);
 
-    car.nome="Tornado";
-    car.cor="Vermelho";
-    car.pot=450;
-    car.vMax=350;
-
-    car2.nome="Monza";
-    car2.cor="Preto";
-    car2.pot=250;
-    car2.vMax=260;
-
-    cout << "Nome.............: " << car.nome << "\n";
-    cout << "Cor..............: " << car.cor << "\n";
-    cout << "Potencia.........: " << car.pot << "\n";
-    cout << "Velocidade maxima: " << car.vMax << "\n";
-
-    cout << "Nome.............: " << car2.nome << "\n";
-    cout << "Cor..............: " << car2.cor << "\n";
-    cout << "Potencia.........: " << car2.pot << "\n";
-    cout << "Velocidade maxima: " << car2.vMax << "\n";
+    car1.mostra();
+    car2.mostra();
 
     return 0;
 }
